fad0bd74afc555a62959cbd15e800966e51afaeb
ab604 <ab604@soton.ac.uk>
2020-07-15 16:40:05 +0100

    add vignette

--- R/colors.R
+++ R/colors.R
@@ -1,6 +1,6 @@
 #' University of Southampton colour palettes
 #' A.Bailey 2020-07-10
-#' 
+#'
 #' A collection of colour palettes using the University of Southampton colour
 #' scheme. The list of available palettes is:
 #' neutral
@@ -9,33 +9,31 @@
 #' contrast1
 #' contrast2
 #' contrast3
-#' 
-#' @examples 
-#' \dontrun{
+#'
+#' Make a named list of University of Southampton brand colours
+#' @examples
 #' #' # Make an x-y plot using the marine palette
 #' library(tidyverse)
-#' df <- data.frame(x = rnorm(100, 0, 20), 
-#'           y = rnorm(100, 0, 20), 
+#' df <- data.frame(x = rnorm(100, 0, 20),
+#'           y = rnorm(100, 0, 20),
 #'           cl = sample(letters[1:8], 100, replace=TRUE))
-#' ggplot(df, aes(x, y, colour=cl, shape=cl)) + 
+#' ggplot(df, aes(x, y, colour=cl, shape=cl)) +
 #'   geom_point(size=4) + scale_colour_uos() +
-#'   theme_bw() + theme(aspect.ratio=1) 
-#' }
+#'   theme_bw() + theme(aspect.ratio=1)
 #' @export
-#' Make a named list of University of Southampton brand colours 
 uos_colours <- c(
-  # Rich-Black, Neutral-1, 
+  # Rich-Black, Neutral-1,
   # Neutral-2, Neutral-3
   'black' = "#00131D",
   'neutral1' = "#495961",
   'neutral2' = "#758D9A",
   'neutral3' = "#9FB1BD",
   'neutral4' = "#E1E8EC",
-  # Marine-1, Marine-2, Marine-3, 
+  # Marine-1, Marine-2, Marine-3,
   # Marine-5, Marine-4, Marine-6
   'marine1' = "#005C84",
   'marine2' = "#74C9E5",
-  'marine3' = "#3CBAC6", 
+  'marine3' = "#3CBAC6",
   'marine4' = "#B3DBD2",
   'marine5' = "#4BB694",
   'marine6' = "#C1D100",
@@ -54,14 +52,14 @@ uos_colours <- c(
 
 #' Function to extract uos_colours as hex codes
 #'
-#' @param ... Character names of uos_colours 
-#'
+#' @param ... Character names of uos_colours
+#' @export
 uos_cols <- function(...) {
   cols <- c(...)
-  
+
   if (is.null(cols))
     return (uos_colours)
-  
+
   uos_colours[cols]
 }
 
